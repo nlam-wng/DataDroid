0ea92f2054aab2973d6fccef40e9301c18bcb49e
Nicole Lam <nicole.lam@worldnomadsgroup.com>
2013-02-27 15:39:29 +1100

    Fix up byte array size

--- DataDroid/src/com/foxykeep/datadroid/internal/network/NetworkConnectionImpl.java
+++ DataDroid/src/com/foxykeep/datadroid/internal/network/NetworkConnectionImpl.java
@@ -323,7 +323,7 @@ public final class NetworkConnectionImpl {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 
         int nRead;
-        byte[] data = new byte[16384];
+        byte[] data = new byte[1024];
 
         while ((nRead = cleanedIs.read(data, 0, data.length)) != -1) {
           buffer.write(data, 0, nRead);
